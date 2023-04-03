1dd236dfe457f4c3192882d338648dc013408ada
Bessgendre <106878709+Bessgendre@users.noreply.github.com>
2023-04-03 21:49:43 +0800

    new chapter: grand canonical ensembles

--- del.py
+++ del.py
@@ -1,6 +1,6 @@
 import os
 files = os.listdir('./')
-del_suffix = ['toc', 'vrb', 'aux', 'log', 'nav', 'out', 'snm', 'synctex.gz']
+del_suffix = ['toc', 'vrb', 'aux', 'log', 'nav', 'out', 'snm', 'synctex.gz','bbl','blg']
 for file in files:
     for suffix in del_suffix:
         if file.endswith(suffix):
