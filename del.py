5a6b6ff8232c17e25bee0f03801f0a6fedb7abdb
Bessgendre <106878709+Bessgendre@users.noreply.github.com>
2023-04-04 16:41:58 +0800

    rebuild the workplace and change the layout

--- del.py
+++ del.py
@@ -1,6 +1,6 @@
 import os
 files = os.listdir('./')
-del_suffix = ['toc', 'vrb', 'aux', 'log', 'nav', 'out', 'snm', 'synctex.gz','bbl','blg']
+del_suffix = ['toc', 'vrb', 'aux', 'log', 'nav', 'out', 'snm', 'synctex.gz','bbl','blg','xml','bcf']
 for file in files:
     for suffix in del_suffix:
         if file.endswith(suffix):
